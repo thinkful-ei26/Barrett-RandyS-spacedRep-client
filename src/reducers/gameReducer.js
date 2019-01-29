0dd409194bc49aa50c19b0efb39c3b5b6643b2fa
randy sartor <randal.sartor@gmail.com>
2019-01-29 15:28:37 -0500

    all components working

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -4,13 +4,15 @@ import { TOGGLE_MODAL } from '../actions/game';
 import { FETCH_WORD_REQUEST,
   FETCH_WORD_SUCCESS,
   FETCH_WORD_ERROR,
+  SAVE_USER_ANSWER,
   fetchWord } from '../actions/index';
 
 const initialState = {
   showModal: false,
   currentWord: null,
   loading: false,
-  error: null
+  error: null,
+  userAnswer: null,
 };
 
 const gameReducer = (state=initialState, action) => {
@@ -23,20 +25,28 @@ const gameReducer = (state=initialState, action) => {
   if (action.type === FETCH_WORD_REQUEST) {
     console.log('request dispatched');
     return Object.assign({}, state, {
+      userAnswer: null,
       loading: true
     })
   } else if (action.type === FETCH_WORD_SUCCESS) {
     return Object.assign({}, state, {
       loading: false,
       currentWord: action.word,
-      error: null
+      error: null,
+      userAnswer: null,
     })
   } else if (action.type === FETCH_WORD_ERROR) {
     return Object.assign({}, state, {
       loading: false,
+      userAnswer: null,
       error: action.error
     })
-  } 
+  } else if (action.type === SAVE_USER_ANSWER) {
+    console.log('action dispatched', action.word)
+    return Object.assign({}, state, {
+      userAnswer: action.word
+    })
+  }
 
   return state
 }
